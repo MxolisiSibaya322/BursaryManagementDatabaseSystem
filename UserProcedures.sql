2b92cc04697283fb92180f060dfad951b8ebe93a
Iviwe Mshumpela <Thuthuzwa.Mshumpela@bbd.co.za>
2024-02-09 11:05:05 +0200

    ammended user procedure

--- UserProcedures.sql
+++ UserProcedures.sql
@@ -1,24 +1,18 @@
 
 CREATE PROCEDURE AddUser
+
     @UserName VARCHAR(50),
-    @RoleID INT,
-    @UniversityID INT
+
+	@Email varchar(120),
+
+    @RoleID int
+
 AS
+
 BEGIN
-    DECLARE @UserID INT
 
-    -- Insert user into Users table
-    INSERT INTO Users (UserName)
-    VALUES (@UserName)
+    INSERT INTO Users (UserName,Email,RoleID)
 
-    -- Get the ID of the newly inserted user
-    SET @UserID = SCOPE_IDENTITY()
+    VALUES (@UserName,@Email,@RoleID)
 
-    -- Insert user's role into UserRoles table
-    INSERT INTO UserRoles (UserID, RoleID)
-    VALUES (@UserID, @RoleID)
-
-    -- Insert user's university into UserUniversities table
-    INSERT INTO UserUniversities (UserID, UniversityID)
-    VALUES (@UserID, @UniversityID)
 END
