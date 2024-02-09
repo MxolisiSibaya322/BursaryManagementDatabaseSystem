644fe2d0db75134dc1e6dd4a08e9b4507a776086
Tebogo Motibana <Tebogo.Motibana@bbd.co.za>
2024-02-09 08:41:28 +0200

    modified stored procedure

--- StoreDataProcedure.sql
+++ StoreDataProcedure.sql
@@ -2,6 +2,6 @@ CREATE PROCEDURE InsertIntoUniversityTable
  @UniversityID int ,
 @AmountAllocated money ,
 @AmountRemaining money ,
-AmountSpent money
-AS INSERT INTO BursaryAllocations
+@AmountSpent money
+AS INSERT INTO BursaryAllocations (UniversityID,AmountAllocated,AmountRemaining,AmountSpent)
  VALUES (@UniversityID,@AmountAllocated,@AmountRemaining,@AmountSpent)
