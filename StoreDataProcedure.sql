bae28930d43cbe6e941767e4cdccaa0837e20e09
tebogomotibane <tebogomotibane@gmail.com>
2024-02-09 15:19:16 +0200

    merged tale files

--- StoreDataProcedure.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROCEDURE InsertIntoUniversityTable
- @UniversityID int ,
-@AmountAllocated money ,
-@AmountRemaining money ,
-@AmountSpent money
-AS INSERT INTO BursaryAllocations (UniversityID,AmountAllocated,AmountRemaining,AmountSpent)
- VALUES (@UniversityID,@AmountAllocated,@AmountRemaining,@AmountSpent)
