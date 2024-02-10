9f083574f9757bbc6406c81102c7e5e855403d3a
Mxolisi Sibaya <Mxolisi.Sibaya@bbd.co.za>
2024-02-10 13:32:03 +0200

    Added gender,department and race tables and fixed corresponding tables to be consistent

--- raceAndGender.sql
+++ raceAndGender.sql
@@ -24,7 +24,49 @@ Department varchar(7)
 );
 GO
 INSERT INTO dbo.Departments(Department) VALUES ('ComSci'),('BEng'),('GameDev')
+GO
+
+
 
 SELECT * FROm [dbo].Ethnicity
 SELECT * FROm [dbo].Genders
 SELECT * FROm [dbo].Departments
+
+CREATE TRIGGER [dbo].no_more_inserts_gender
+ON [dbo].Genders
+AFTER INSERT
+AS
+BEGIN
+  IF (SELECT COUNT(*) FROM [dbo].Genders) > 2 
+  BEGIN
+    ROLLBACK TRANSACTION
+    RAISERROR ('You cannot insert into gender table.', 16, 1);
+  END
+END
+GO
+
+CREATE TRIGGER [dbo].no_more_inserts_departments
+ON [dbo].Departments
+AFTER INSERT
+AS
+BEGIN
+  IF (SELECT COUNT(*) FROM [dbo].Departments) > 3
+  BEGIN
+    ROLLBACK TRANSACTION
+    RAISERROR ('You cannot insert into Departments table.', 16, 1);
+  END
+END
+GO
+
+CREATE TRIGGER [dbo].no_more_inserts_race
+ON  [dbo].Ethnicity
+AFTER INSERT
+AS
+BEGIN
+  IF (SELECT COUNT(*) FROM [dbo].Ethnicity) > 3
+  BEGIN
+    ROLLBACK TRANSACTION
+    RAISERROR ('You cannot insert into Race table.', 16, 1);
+  END
+END
+GO
