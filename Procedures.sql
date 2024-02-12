f60f57bf53041e3a3a5f8bce9cac8c0ea120fa3f
Mxolisi Sibaya <Mxolisi.Sibaya@bbd.co.za>
2024-02-12 08:16:19 +0200

    Changed the script to generate mock data, updated procedures as well as views

--- Procedures.sql
+++ Procedures.sql
@@ -1,3 +1,65 @@
+CREATE PROCEDURE AddStudentAllocation
+    @FirstName VARCHAR(255),
+    @LastName VARCHAR(255),
+    @Email VARCHAR(255),
+    @PhoneNumber VARCHAR(20),
+    @GenderID INT,
+    @DateOfBirth DATE,
+    @EthnicityID INT,
+    @Amount MONEY,
+    @AllocationYear INT,
+    @DeptID INT,
+    @UniversityID INT  -- New parameter
+
+AS
+BEGIN
+    -- Suppress row count for affected rows
+    SET NOCOUNT ON;
+    
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    DECLARE @UserID INT;
+    DECLARE @StudentID INT;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        -- Insert into Users table
+        INSERT INTO dbo.Users (FirstName, LastName, RoleID)
+        VALUES (@FirstName, @LastName, 3); -- Assuming RoleID 3 is for students
+
+        -- Get the generated UserID
+        SET @UserID = SCOPE_IDENTITY(); 
+
+        -- Insert into ContactDetails table
+        INSERT INTO ContactDetails (UserID, Email, PhoneNumber)
+        VALUES (@UserID, @Email, @PhoneNumber);
+
+        -- Insert into StudentsTable
+        INSERT INTO dbo.StudentsTable (UserID, DateOfBirth, EthnicityID, DepartmentID, UniversityID, GenderID)
+        VALUES (@UserID, @DateOfBirth, @EthnicityID, @DeptID, @UniversityID, @GenderID);
+
+        -- Get the generated StudentID
+        SET @StudentID = SCOPE_IDENTITY(); 
+
+        -- Insert allocation into StudentAllocations
+        INSERT INTO dbo.StudentAllocations (Amount,AllocationYear, StudentID)
+        VALUES (@Amount,@AllocationYear, @StudentID);
+
+        -- Commit the transaction if all operations succeed
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        -- Rollback the transaction if an error occurs
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+
+        SET @ErrorMessage = 'Error: Invalid value provided. Please provide valid input values.';
+        RAISERROR(@ErrorMessage, 16, 1); -- Use severity level 16 for non-fatal errors
+    END CATCH;
+END;
+
+
+
 CREATE PROCEDURE AddRole @RoleName varchar(3)
 AS
 BEGIN 
